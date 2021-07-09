7fabe6338e8675ebf5d1b593a57f67f138a8bff1
roger <a1063333@mail.nuk.edu.tw>
2021-07-09 16:06:01 +0800

    PagingDataAdapter

--- app/src/main/java/com/example/intern_project1/view/adapter/FactoryInfoAdapter.kt
+++ app/src/main/java/com/example/intern_project1/view/adapter/FactoryInfoAdapter.kt
@@ -3,44 +3,36 @@ package com.example.intern_project1.view.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.intern_project1.model.network.entities.entities.FactoryObject
 import com.example.intern_project1.databinding.ItemFactoryListLayoutBinding
+import com.example.intern_project1.view.viewholder.FactoryViewHolder
 
-class FactoryInfoAdapter(val context: Context, val items: ArrayList<FactoryObject.DataX>) :
-    RecyclerView.Adapter<FactoryInfoAdapter.ViewHolder>() {
+class FactoryInfoAdapter : PagingDataAdapter<FactoryObject.DataX, FactoryViewHolder>(COMPARATOR) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val mbinding: ItemFactoryListLayoutBinding =
-            ItemFactoryListLayoutBinding.inflate(LayoutInflater.from(context), parent, false)
-        return ViewHolder(mbinding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FactoryViewHolder {
+        return FactoryViewHolder.create(
+            parent
+        )
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-        val item = items.get(position)
-
-        holder.tvCity.text = item.city
-        holder.tvTitle.text = item.name
-        holder.tvAddress.text = item.address
-        holder.tvPhone.text = item.phone
-        holder.tvBrandName.text = item.tags
-
+    override fun onBindViewHolder(holder: FactoryViewHolder, position: Int) {
+        getItem(position)?.let {
+            holder.bind(it)
+        }
     }
 
-    override fun getItemCount(): Int {
-        return items.size
+    companion object {
+        private val COMPARATOR = object : DiffUtil.ItemCallback<FactoryObject.DataX>() {
+            override fun areItemsTheSame(oldItem: FactoryObject.DataX, newItem: FactoryObject.DataX): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: FactoryObject.DataX, newItem: FactoryObject.DataX): Boolean {
+                return oldItem == newItem
+            }
+        }
     }
-
-    class ViewHolder(view: ItemFactoryListLayoutBinding) : RecyclerView.ViewHolder(view.root) {
-
-        val ivFactory = view.ivFactoryImage
-        val tvCity = view.tvCity
-        val tvTitle = view.tvTitle
-        val tvAddress = view.tvAddress
-        val tvPhone = view.tvPhone
-        val tvBrandName = view.tvBrandname
-
-    }
-
 }
---
+++ app/src/main/java/com/example/intern_project1/view/viewholder/FactoryViewHolder.kt
@@ -0,0 +1,36 @@
+package com.example.intern_project1.view.viewholder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.intern_project1.R
+import com.example.intern_project1.databinding.ItemFactoryListLayoutBinding
+import com.example.intern_project1.model.network.entities.entities.FactoryObject
+
+class FactoryViewHolder(private val mbinding: ItemFactoryListLayoutBinding) : RecyclerView.ViewHolder(mbinding.root) {
+
+    private var factoryInfo: FactoryObject.DataX? = null
+
+    fun bind(factoryInfo: FactoryObject.DataX) {
+        this.factoryInfo=factoryInfo
+
+        mbinding.tvCity.text = factoryInfo.city
+        mbinding.tvTitle.text = factoryInfo.name
+        mbinding.tvAddress.text = factoryInfo.address
+        mbinding.tvPhone.text = factoryInfo.phone
+        mbinding.tvBrandname.text = factoryInfo.tags
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): FactoryViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_factory_list_layout,  parent,false)
+
+            val binding = ItemFactoryListLayoutBinding.bind(view)
+
+            return FactoryViewHolder(
+                binding
+            )
+        }
+    }
+}
