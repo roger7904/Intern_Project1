26eea5a64b49fc07070bdd4c8d1ebf3b09e0fc1f
roger <a1063333@mail.nuk.edu.tw>
2021-07-19 17:39:33 +0800

    add data to map

--- app/src/main/java/com/example/intern_project1/MapActivity.kt
+++ app/src/main/java/com/example/intern_project1/MapActivity.kt
@@ -1,14 +1,28 @@
 package com.example.intern_project1.view.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.intern_project1.BitmapHelper
 import com.example.intern_project1.MarkerInfoWindowAdapter
 import com.example.intern_project1.R
+import com.example.intern_project1.databinding.ActivityMainBinding
+import com.example.intern_project1.databinding.ActivityMapBinding
+import com.example.intern_project1.model.network.entities.entities.FactoryObject
 import com.example.intern_project1.place.Place
 import com.example.intern_project1.place.PlaceRenderer
 import com.example.intern_project1.place.PlacesReader
+import com.example.intern_project1.utils.Injection
+import com.example.intern_project1.viewmodel.FactoryViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,9 +30,25 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.ktx.addMarker
+import com.google.android.libraries.places.api.net.PlacesClient
 
 
 class MapActivity : AppCompatActivity() , OnMapReadyCallback {
+
+    private lateinit var mbinding: ActivityMapBinding
+    private lateinit var mFactoryViewModel: FactoryViewModel
+
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var locationPermissionGranted = false
+
+    // The entry point to the Fused Location Provider.
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private var lastKnownLocation: Location? = null
 
     private val places: List<Place> by lazy {
         PlacesReader(this).read()
@@ -27,7 +57,18 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
     // [START maps_android_add_map_codelab_ktx_coroutines]
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+
+        mbinding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
+
+        mFactoryViewModel = ViewModelProvider(this, Injection.provideFactoryViewModel(this)).get(
+            FactoryViewModel::class.java)
+
+        mFactoryViewModel.getFactoryInfoFromApi()
+
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         val mapFragment =
             supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -56,9 +97,81 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
     // [END maps_android_add_map_codelab_ktx_coroutines]
 
     /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready for use.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        //addClusteredMarkers(googleMap)
+
+        factoryViewModelObserver(googleMap)
+
+        googleMap.setMapStyle(
+            MapStyleOptions.loadRawResourceStyle(
+                this, R.raw.style
+            )
+        )
+
+//        googleMap.setOnMapLoadedCallback {
+//            // Ensure all places are visible in the map
+//            val bounds = LatLngBounds.builder()
+//            places.forEach { bounds.include(it.latLng) }
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+//        }
+
+//        val australiaBounds = LatLngBounds(
+//            LatLng((-44.0), 113.0),  // SW bounds
+//            LatLng((-10.0), 154.0) // NE bounds
+//        )
+//        map.moveCamera(CameraUpdateFactory.newLatLngBounds(australiaBounds, 0))
+
+        // Position the map's camera near Sydney, Australia.
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(-34, 151)))
+
+        // Prompt the user for permission.
+        getLocationPermission()
+        // [END_EXCLUDE]
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI(googleMap)
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation(googleMap)
+    }
+
+    private fun factoryViewModelObserver(googleMap: GoogleMap) {
+
+        mFactoryViewModel.response.observe(
+            this,
+            Observer { response ->
+                response?.let {
+
+                    addMarkers(googleMap,response.data.data)
+
+                    Log.i("FactoryInfo Response", "${response.data.data[0]}")
+                }
+            })
+
+        mFactoryViewModel.loadingError.observe(
+            this,
+            Observer { dataError ->
+                dataError?.let {
+                    Log.i("API Error", "$dataError")
+                }
+            })
+
+        mFactoryViewModel.loading.observe(this,
+            Observer { loading ->
+                loading?.let {
+                    Log.i("Loading", "$loading")
+                }
+            })
+    }
+
+    /**
      * Adds markers to the map with clustering support.
      */
-    private fun addClusteredMarkers(googleMap: GoogleMap) {
+    private fun addClusteredMarkers(googleMap: GoogleMap,factoryPlace: List<FactoryObject.DataX>) {
         // Create the ClusterManager class and set the custom renderer
         val clusterManager = ClusterManager<Place>(this, googleMap)
         clusterManager.renderer =
@@ -124,56 +237,129 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
     /**
      * Adds markers to the map. These markers won't be clustered.
      */
-    private fun addMarkers(googleMap: GoogleMap) {
-        places.forEach { place ->
+    private fun addMarkers(googleMap: GoogleMap,factoryPlace: List<FactoryObject.DataX>) {
+        factoryPlace.forEach { result ->
             val marker = googleMap.addMarker {
-                title(place.name)
-                position(place.latLng)
+                title(result.name)
+                position(LatLng(result.latitude.toDouble(),result.longitude.toDouble()))
                 icon(bicycleIcon)
             }
             // Set place as the tag on the marker object so it can be referenced within
             // MarkerInfoWindowAdapter
-            marker.tag = place
+            marker.tag = result
+            googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
         }
     }
+
+
     // [END maps_android_add_map_codelab_ktx_add_markers]
 
-    companion object {
-        val TAG = MapActivity::class.java.simpleName
+    /**
+     * Prompts the user for permission to use the device location.
+     */
+    // [START maps_current_place_location_permission]
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
     }
 
     /**
-     * Manipulates the map when it's available.
-     * The API invokes this callback when the map is ready for use.
+     * Handles the result of the request for location permissions.
      */
+    // [START maps_current_place_on_request_permissions_result]
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        locationPermissionGranted = false
 
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        
-        addClusteredMarkers(googleMap)
-
-        googleMap.setMapStyle(
-            MapStyleOptions.loadRawResourceStyle(
-                this, R.raw.style
-            )
-        )
-
-        googleMap.setOnMapLoadedCallback {
-            // Ensure all places are visible in the map
-            val bounds = LatLngBounds.builder()
-            places.forEach { bounds.include(it.latLng) }
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
         }
-
-//        val australiaBounds = LatLngBounds(
-//            LatLng((-44.0), 113.0),  // SW bounds
-//            LatLng((-10.0), 154.0) // NE bounds
-//        )
-//        map.moveCamera(CameraUpdateFactory.newLatLngBounds(australiaBounds, 0))
-
-        // Position the map's camera near Sydney, Australia.
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(-34, 151)))
+        //updateLocationUI()
     }
 
+    /**
+     * Updates the map's UI settings based on whether the user has granted location permission.
+     */
+    // [START maps_current_place_update_location_ui]
+    private fun updateLocationUI(map :GoogleMap) {
+        if (map == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
 
+    /**
+     * Gets the current location of the device, and positions the map's camera.
+     */
+    // [START maps_current_place_get_device_location]
+    private fun getDeviceLocation(map :GoogleMap) {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            map?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                            Log.i(TAG, "Current location is "
+                                    +lastKnownLocation!!.latitude+lastKnownLocation!!.longitude)
+                        }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        map?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    companion object {
+        private val TAG = MapActivity::class.java.simpleName
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    }
 }
--- app/src/main/java/com/example/intern_project1/MarkerInfoWindowAdapter.kt
+++ app/src/main/java/com/example/intern_project1/MarkerInfoWindowAdapter.kt
@@ -18,20 +18,20 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
-import com.example.intern_project1.place.Place
+import com.example.intern_project1.model.network.entities.entities.FactoryObject
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 
 class MarkerInfoWindowAdapter(private val context: Context) : GoogleMap.InfoWindowAdapter {
     override fun getInfoContents(marker: Marker?): View? {
         // 1. Get tag
-        val place = marker?.tag as? Place ?: return null
+        val factoryInfo = marker?.tag as? FactoryObject.DataX ?: return null
 
         // 2. Inflate view and set title, address and rating
         val view = LayoutInflater.from(context).inflate(R.layout.marker_info_contents, null)
-        view.findViewById<TextView>(R.id.text_view_title).text = place.name
-        view.findViewById<TextView>(R.id.text_view_address).text = place.address
-        view.findViewById<TextView>(R.id.text_view_rating).text = "Rating: %.2f".format(place.rating)
+        view.findViewById<TextView>(R.id.tv_title).text = factoryInfo.name
+        view.findViewById<TextView>(R.id.tv_address).text = factoryInfo.address
+        view.findViewById<TextView>(R.id.tv_phone).text = factoryInfo.phone
 
         return view
     }
